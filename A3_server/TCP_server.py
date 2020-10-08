6fd1645069278119229a810af04e2611487efced
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2020-10-08 15:33:28 +0200

    Initial TCP server commit.

---
+++ A3_server/TCP_server.py
@@ -0,0 +1,75 @@
+import socket
+import threading
+import random
+import string
+
+SERVER_PORT = 80
+
+client_dict = {}
+
+def create_UID():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+
+def read_one_line(sock):
+    """
+    Read one line of text from a socket
+    :param sock: The socket to read from.
+    :return:
+    """
+    newline_received = False
+    message = ""
+    while not newline_received:
+        character = sock.recv(1).decode()
+        if character == '\n':
+            newline_received = True
+        elif character == '\r':
+            pass
+        else:
+            message += character
+    return message
+
+def handle_next_client(connection_socket, client_id):
+    command = 'x'
+    while command != "":
+        command = read_one_line(connection_socket)
+        print(f"Client ID '{client_id}': {command}")
+        if command == "ping":
+            response = "PONG"
+        else:
+            response = "501 Command not implemented or 404 command doesn't exist."
+        connection_socket.send(response.encode())
+
+    connection_socket.close()
+
+
+def start_server():
+    welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    welcome_socket.bind(("", SERVER_PORT))
+    welcome_socket.listen(1)
+
+    hostname = socket.gethostname()
+    HOST_ADDRESS = socket.gethostbyname(hostname)
+    print(f"Server ready for client connections at (local IPA) {HOST_ADDRESS}:{SERVER_PORT}")
+    need_to_run = True
+
+
+    while need_to_run:
+        connection_socket, client_address = welcome_socket.accept()
+
+        client_id = create_UID()
+        while client_id in client_dict:
+            client_id = create_UID()
+        client_dict[client_id] = {}
+        print(client_dict)
+
+        print(f"Client ID '{client_id}' connected.")
+        client_thread = threading.Thread(target=handle_next_client,
+                                         args=(connection_socket, client_id))
+        client_thread.start()
+
+    welcome_socket.close()
+    print("Server shutdown")
+
+
+if __name__ == "__main__":
+    start_server()
---
+++ A3_server/assets/config.py
@@ -0,0 +1,33 @@
+import argparse
+
+# Enable for DEBUG printouts
+DEBUG = 0
+
+
+TCP_PORT = 1300                     # TCP port used for communication
+SERVER_HOST = "datakomm.work"       # Set this to either hostname (domain) or IP address of the chat server
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug",
+                    help="Set True for debug printouts. Default: False",
+                    type=bool, metavar='')
+parser.add_argument("-p", "--port",
+                    help="Port number to use for connection. Default: 1300",
+                    type=int, metavar='')
+parser.add_argument("-a", "--address",
+                    help="Host server IP address or domain name address. Default: 'datakomm.work'",
+                    metavar='')
+args = parser.parse_args()
+
+if args.debug:
+    DEBUG = args.debug
+    print(f"Debug printouts are set to: {DEBUG}")
+
+if args.port:
+    TCP_PORT = args.port
+    print(f"Port used: {TCP_PORT}")
+
+if args.address:
+    SERVER_HOST = args.address
+    print(f"Host server address (domain or IP): {SERVER_HOST}")
