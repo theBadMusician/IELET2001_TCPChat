85071a2e027f2dfa8d7617fec1afab6b8f20e83a
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2020-10-08 22:43:17 +0200

    Fully supported sync mode. Fixed a crashing bug in jokes.txt.

--- A3_server/TCP_server.py
+++ A3_server/TCP_server.py
@@ -42,9 +42,6 @@ def read_one_line(sock):
             message += character
     return message
 
-def handle_sync_mode():
-    hey = 1
-
 def handle_login_req(client_id, login_req_parsed):
     username = login_req_parsed[1]
 
@@ -63,25 +60,54 @@ def handle_login_req(client_id, login_req_parsed):
 
 def handle_msg_req(client_id, msg_req_parsed):
     msg = msg_req_parsed[1]
-    try:
-        return f"msgok {len(client_dict) - 1}\n", f"msg {client_dict[client_id]['client_name']} {msg}\n"
-    except Exception:
-        return "msgerror Something happened ¯\\_(ツ)_/¯\n"
+
+    response_string = f"msg {client_dict[client_id]['client_name']} {msg}\n"
+    for dict_client_id in client_dict.keys():
+        if client_id == dict_client_id:
+            continue
+        elif client_dict[dict_client_id]['sync_mode']:
+            try:
+                client_dict[dict_client_id]['inbox'].append(response_string)
+                if DEBUG:
+                    print("Client dict: " + str(client_dict[dict_client_id]))
+                return f"msgok {len(client_dict) - 1}\n"
+            except Exception:
+                return "msgerror Something happened ¯\\_(ツ)_/¯\n"
+        else:
+            try:
+                client_dict[dict_client_id]['socket'].send(response_string.encode())
+                return f"msgok {len(client_dict) - 1}\n"
+            except Exception:
+                return "msgerror Something happened ¯\\_(ツ)_/¯\n"
 
 def handle_privmsg_req(client_id, msg_req_parsed):
     recipient_msg = msg_req_parsed[1].split(' ', 1)
+
     recipient = recipient_msg[0]
+    recipient_id = [k for k, v in client_dict.items() if recipient in v['client_name']][0]
+
     msg = recipient_msg[1]
+
+    response_string = f"privmsg {client_dict[client_id]['client_name']} {msg}\n"
 
     if client_dict[client_id]['client_name'] == "Anon-" + str(client_id):
         return "msgerr unauthorized\n"
     elif not any(recipient in name['client_name'] for name in client_dict.values()):
         return "msgerr incorrect recipient\n"
-    elif any(recipient in name['client_name'] for name in client_dict.values()) and "Anon-" in recipient:
+    elif recipient == "Anon-" + str(recipient_id):
         return "msgerr incorrect recipient\n"
+    elif client_dict[recipient_id]['sync_mode']:
+        try:
+            client_dict[recipient_id]['inbox'].append(response_string)
+            if DEBUG:
+                print("Client dict: " + str(client_dict[dict_client_id]))
+            return f"msgok 1\n"
+        except Exception:
+            return "msgerror Something happened ¯\\_(ツ)_/¯\n"
     else:
         try:
-            return f"msgok 1\n", f"privmsg {client_dict[client_id]['client_name']} {msg}\n"
+            client_dict[recipient_id]['socket'].send(response_string.encode())
+            return f"msgok 1\n"
         except Exception:
             return "msgerror Something happened ¯\\_(ツ)_/¯\n"
 
@@ -102,24 +128,24 @@ def handle_next_client(connection_socket, client_id):
             response = handle_login_req(client_id, command_parsed)
         elif command_parsed[0] == "msg":
             response = handle_msg_req(client_id, command_parsed)
-            [client_dict[dict_client_id]['socket'].send(response[1].encode()) for dict_client_id in client_dict.keys() if client_id != dict_client_id]
         elif command_parsed[0] == "privmsg":
-            recipient = command_parsed[1].split(' ', 1)[0]
             response = handle_privmsg_req(client_id, command_parsed)
-            recipient_id = [k for k, v in client_dict.items() if recipient in v['client_name']][0]
-            client_dict[recipient_id]['socket'].send(response[1].encode())
+        elif command == "inbox":
+            client_inbox = client_dict[client_id]['inbox']
+            inbox_contents = ''.join([str(elem) for elem in client_inbox])
+            response = "inbox " + str(len(client_inbox)) + "\n" + str(inbox_contents)
+            client_dict[client_id]['inbox'] = []
         elif command_parsed[0] == "users":
             client_names_list = [v['client_name'] for k, v in client_dict.items()]
             client_names = ' '.join([str(elem) for elem in client_names_list])
             response = "users " + client_names + "\n"
         elif command == "joke":
-            response = "joke " + joke_list[random.randint(0, len(joke_list) - 1)]
+            joke = joke_list[random.randint(0, len(joke_list) - 1)]
+            response = "joke " + joke
         else:
             response = "501 Command not implemented or 404 command doesn't exist."
-        try:
-            connection_socket.send(response.encode())
-        except AttributeError:
-            connection_socket.send(response[0].encode())
+
+        connection_socket.send(response.encode())
 
     connection_socket.close()
 
@@ -144,6 +170,7 @@ def start_server():
         client_dict[client_id]['client_name'] = "Anon-" + client_id
         client_dict[client_id]['client_address'] = client_address
         client_dict[client_id]['socket'] = connection_socket
+        client_dict[client_id]['sync_mode'] = False
 
         if DEBUG:
             print("Whole client dict: " + str(client_dict))
