daa55aab383afaf3489436e68b50d9614f5f87ff
Benjaminas Visockis <benjamvi@stud.ntnu.no>
2020-10-09 13:51:22 +0200

    Added an OK working disconnect handling solution.

--- A3_server/TCP_server.py
+++ A3_server/TCP_server.py
@@ -1,4 +1,5 @@
 from assets.config import *
+from assets.DebugTools import *
 import socket
 import threading
 import random
@@ -7,19 +8,12 @@ import string
 SERVER_PORT = 80
 
 client_dict = {}
+cmd_list = ['sync', 'async', 'login', 'msg', 'privmsg', 'inbox', 'users', 'joke', 'help']
 joke_list = []
 
 with open("assets/jokes.txt", "r") as jokes:
     for line in jokes:
         joke_list.append(line)
-
-def deep_get(_dict, keys, default=None):
-    for key in keys:
-        if isinstance(_dict, dict):
-            _dict = _dict.get(key, default)
-        else:
-            return default
-    return _dict
 
 def create_UID():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
@@ -33,7 +27,10 @@ def read_one_line(sock):
     newline_received = False
     message = ""
     while not newline_received:
-        character = sock.recv(1).decode()
+        try:
+            character = sock.recv(1).decode()
+        except (ConnectionResetError, ConnectionAbortedError) as e:
+            return -1
         if character == '\n':
             newline_received = True
         elif character == '\r':
@@ -41,6 +38,25 @@ def read_one_line(sock):
         else:
             message += character
     return message
+
+def dc_checkup(client_id):
+
+    for dict_client_id in tuple(client_dict):
+        if client_id != dict_client_id:
+            try:
+                client_dict[dict_client_id]['socket'].send("\r".encode())
+                WARNING_PRINT("SENT CHECK TO " + dict_client_id)
+            except (ConnectionResetError, ConnectionAbortedError) as e:
+                client_dict[dict_client_id]['socket'].close()
+        print("<<<<================>")
+
+def dc_handling_cleanup():
+    dead_sockets = [k for k, v in client_dict.items() if v['socket'].fileno() == -1]
+    for k in dead_sockets:
+        client_dict.pop(k, None)
+        WARNING_PRINT(f"SOCKET {k} CLOSED")
+        print(client_dict)
+    dead_sockets.clear()
 
 def handle_login_req(client_id, login_req_parsed):
     username = login_req_parsed[1]
@@ -54,38 +70,32 @@ def handle_login_req(client_id, login_req_parsed):
     # Set username in db and send res
     else:
         client_dict[client_id]['client_name'] = username
-        if DEBUG:
-            print("Client dict: " + str(client_dict[client_id]))
         return "loginok\n"
 
 def handle_msg_req(client_id, msg_req_parsed):
     msg = msg_req_parsed[1]
 
     response_string = f"msg {client_dict[client_id]['client_name']} {msg}\n"
-    for dict_client_id in client_dict.keys():
-        if client_id == dict_client_id:
-            continue
-        elif client_dict[dict_client_id]['sync_mode']:
-            try:
+    try:
+        for dict_client_id in client_dict.keys():
+            if client_id == dict_client_id:
+                continue
+            elif client_dict[dict_client_id]['sync_mode']:
                 client_dict[dict_client_id]['inbox'].append(response_string)
-                if DEBUG:
-                    print("Client dict: " + str(client_dict[dict_client_id]))
-                return f"msgok {len(client_dict) - 1}\n"
-            except Exception:
-                return "msgerror Something happened ¯\\_(ツ)_/¯\n"
-        else:
-            try:
+            else:
                 client_dict[dict_client_id]['socket'].send(response_string.encode())
-                return f"msgok {len(client_dict) - 1}\n"
-            except Exception:
-                return "msgerror Something happened ¯\\_(ツ)_/¯\n"
+        return f"msgok {len(client_dict) - 1}\n"
+    except Exception:
+        return "msgerror Something happened ¯\\_(ツ)_/¯\n"
 
 def handle_privmsg_req(client_id, msg_req_parsed):
     recipient_msg = msg_req_parsed[1].split(' ', 1)
 
     recipient = recipient_msg[0]
-    recipient_id = [k for k, v in client_dict.items() if recipient in v['client_name']][0]
-
+    try:
+        recipient_id = [k for k, v in client_dict.items() if recipient in v['client_name']][0]
+    except IndexError:
+        return "msgerr incorrect recipient\n"
     msg = recipient_msg[1]
 
     response_string = f"privmsg {client_dict[client_id]['client_name']} {msg}\n"
@@ -99,8 +109,6 @@ def handle_privmsg_req(client_id, msg_req_parsed):
     elif client_dict[recipient_id]['sync_mode']:
         try:
             client_dict[recipient_id]['inbox'].append(response_string)
-            if DEBUG:
-                print("Client dict: " + str(client_dict[dict_client_id]))
             return f"msgok 1\n"
         except Exception:
             return "msgerror Something happened ¯\\_(ツ)_/¯\n"
@@ -111,24 +119,37 @@ def handle_privmsg_req(client_id, msg_req_parsed):
         except Exception:
             return "msgerror Something happened ¯\\_(ツ)_/¯\n"
 
-
 def handle_next_client(connection_socket, client_id):
-    command = 'x'
-    while command != "":
+    socket_open = True
+    while socket_open:
         command = read_one_line(connection_socket)
-        command_parsed = command.split(' ', 1)
+        try:
+            command_parsed = command.split(' ', 1)
+        except AttributeError:
+            pass
         print(f"Client ID '{client_id}': {command}")
-        if command == "ping":
-            response = "PONG\n"
+
+        if command == -1:
+            socket_open = False
+            connection_socket.close()
+            dc_handling_cleanup()
+            continue
         elif command == "sync":
             client_dict[client_id]['sync_mode'] = True
             client_dict[client_id]['inbox'] = []
             response = "modeok\n"
+        elif command == "async":
+            client_dict[client_id]['sync_mode'] = False
+            client_dict[client_id]['inbox'] = []
+            response = "modeok\n"
         elif command_parsed[0] == "login":
+            dc_checkup(client_id)
             response = handle_login_req(client_id, command_parsed)
         elif command_parsed[0] == "msg":
+            dc_checkup(client_id)
             response = handle_msg_req(client_id, command_parsed)
         elif command_parsed[0] == "privmsg":
+            dc_checkup(client_id)
             response = handle_privmsg_req(client_id, command_parsed)
         elif command == "inbox":
             client_inbox = client_dict[client_id]['inbox']
@@ -136,32 +157,43 @@ def handle_next_client(connection_socket, client_id):
             response = "inbox " + str(len(client_inbox)) + "\n" + str(inbox_contents)
             client_dict[client_id]['inbox'] = []
         elif command_parsed[0] == "users":
+            # TODO: fix the bug of users not updating at once. Maybe w setInterval solution.
+            dc_checkup(client_id)
             client_names_list = [v['client_name'] for k, v in client_dict.items()]
             client_names = ' '.join([str(elem) for elem in client_names_list])
             response = "users " + client_names + "\n"
         elif command == "joke":
             joke = joke_list[random.randint(0, len(joke_list) - 1)]
             response = "joke " + joke
+        elif command == "help":
+            cmds = ' '.join([str(elem) for elem in cmd_list])
+            response = "supported " + cmds + "\n"
         else:
-            response = "501 Command not implemented or 404 command doesn't exist."
-
+            response = "cmderr command not supported"
         connection_socket.send(response.encode())
-
-    connection_socket.close()
+        """except (ConnectionResetError, ConnectionAbortedError) as e:
+            connection_socket.close()
+            sockets = [k for k, v in client_dict.items() if v['socket'].fileno() == -1]
+            for k in sockets:
+                socket_open = False
+                client_dict.pop(k, None)
+                WARNING_PRINT(f"SOCKET {k} CLOSED")
+                print(client_dict)
+            sockets.clear()"""
 
 
 def start_server():
-    welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    welcome_socket.bind(("", SERVER_PORT))
-    welcome_socket.listen(1)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("", SERVER_PORT))
+    server_socket.listen(1)
 
     hostname = socket.gethostname()
     HOST_ADDRESS = socket.gethostbyname(hostname)
-    print(f"Server ready for client connections at (local IPA) {HOST_ADDRESS}:{SERVER_PORT}")
-    need_to_run = True
+    print(f"Server ready for client connections at {HOST_ADDRESS}:{SERVER_PORT}")
+    running = True
 
-    while need_to_run:
-        connection_socket, client_address = welcome_socket.accept()
+    while running:
+        connection_socket, client_address = server_socket.accept()
 
         client_id = create_UID()
         while client_id in client_dict.keys():
@@ -172,14 +204,12 @@ def start_server():
         client_dict[client_id]['socket'] = connection_socket
         client_dict[client_id]['sync_mode'] = False
 
-        if DEBUG:
-            print("Whole client dict: " + str(client_dict))
         print(f"Client ID '{client_id}' connected.")
         client_thread = threading.Thread(target=handle_next_client,
                                          args=(connection_socket, client_id))
         client_thread.start()
 
-    welcome_socket.close()
+    server_socket.close()
     print("Server shutdown")
 
 
